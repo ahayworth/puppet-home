99d5b9c1a17abdf71777f6c2d89b832c79ee72a4
Andrew Hayworth <ahayworth@gmail.com>
2018-11-23 17:21:23 -0600

    Fix z-wave and don't prmpt for passwords

--- puppet.sh
+++ puppet.sh
@@ -3,14 +3,17 @@
 if [[ ! -z "$DEBUG" ]]; then debug='--debug'; fi
 
 function decrypt() {
-  gpg -q -d $1 > $1.plaintext
+  gpg --batch --passphrase $passphrase -d $1 > $1.plaintext
   mv $1.plaintext $1
 }
 
 function encrypt() {
-  gpg -c $1
+  gpg -c --batch --passphrase $passphrase $1
   mv $1.gpg $1
 }
+
+read -s -p "Encryption passphrase: " passphrase
+echo
 
 decrypt "modules/home_assistant/files/home_assistant/config/secrets.yaml"
 decrypt "data/secrets.yaml"
@@ -23,8 +26,6 @@ sudo /opt/puppetlabs/bin/puppet \
   --hiera_config=hiera.yaml \
   --show_diff \
   nodes/site.pp
-
-read -p "Press enter to re-encrypt..."
 
 encrypt "modules/home_assistant/files/home_assistant/config/secrets.yaml"
 encrypt "data/secrets.yaml"
