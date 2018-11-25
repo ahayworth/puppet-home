b1aa181c3003a33e3d09b3cd3d596e7124525512
Andrew Hayworth <ahayworth@gmail.com>
2018-11-24 22:17:47 -0600

    Put OZW_Log.txt behind a named pipe and syslog

--- modules/home_assistant/manifests/config.pp
+++ modules/home_assistant/manifests/config.pp
@@ -13,8 +13,14 @@ class home_assistant::config(
     require => File[$config_dir],
   }
   file { "${config_dir}/custom_components/sensor/awair.py":
-    ensure  => present,
-    source  => 'https://raw.github.com/ahayworth/home-assistant/awair/homeassistant/components/sensor/awair.py',
-    require => File[$config_dir],
+    ensure         => present,
+    source         => 'https://raw.github.com/ahayworth/home-assistant/awair/homeassistant/components/sensor/awair.py',
+    require        => File[$config_dir],
+    checksum_value => 'daf1cc0e46f9b4e68a08ce18002ce37e',
+  }
+  exec { "mkfifo_${config_dir}/OZW_Log.txt":
+    command => "/usr/bin/mkfifo ${config_dir}/OZW_Log.txt",
+    creates => "${config_dir}/OZW_Log.txt",
+    require =>  Service['rsyslog'],
   }
 }
