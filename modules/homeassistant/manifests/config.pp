94557f274f495dd1555d086e1bb097a8efcc6cf9
Andrew Hayworth <ahayworth@gmail.com>
2018-11-28 08:54:51 -0600

    update remote configs

--- modules/homeassistant/manifests/config.pp
+++ modules/homeassistant/manifests/config.pp
@@ -8,16 +8,16 @@ class homeassistant::config(
     source  => 'puppet:///modules/homeassistant/homeassistant/config',
     require => File[$homeassistant_dir],
   }
-  file { "${config_dir}/custom_components/sensor/awair.py":
-    ensure         => present,
-    source         => 'https://raw.github.com/ahayworth/home-assistant/awair/homeassistant/components/sensor/awair.py',
-    require        => File[$config_dir],
-    checksum_value => 'daf1cc0e46f9b4e68a08ce18002ce37e',
-  }
   file { "${config_dir}/custom_components/lock/zwave.py":
     ensure         => present,
     #checksum_value => 'daeee9db12bc830c68086e58cdd2f0f0',
     source         => 'https://raw.github.com/ahayworth/home-assistant/ahayworth-schlage-locks/homeassistant/components/lock/zwave.py',
+    require        => File[$config_dir],
+  }
+  file { "${config_dir}/custom_components/media_player/onkyo.py":
+    ensure         => present,
+    #checksum_value => 'daeee9db12bc830c68086e58cdd2f0f0',
+    source         => 'https://raw.github.com/ahayworth/home-assistant/dev/homeassistant/components/media_player/onkyo.py',
     require        => File[$config_dir],
   }
 }
