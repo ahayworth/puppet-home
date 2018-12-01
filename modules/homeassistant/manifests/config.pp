53f091acddeb5a2868414dcb217b10019f210376
Andrew Hayworth <ahayworth@gmail.com>
2018-12-01 09:44:48 -0600

    add custom google trait component

--- modules/homeassistant/manifests/config.pp
+++ modules/homeassistant/manifests/config.pp
@@ -10,7 +10,7 @@ class homeassistant::config(
   }
   file { "${config_dir}/custom_components/lock/zwave.py":
     ensure         => present,
-    #checksum_value => 'daeee9db12bc830c68086e58cdd2f0f0',
+    checksum_value => '0e965d96e8fa0a886f2c3e7234965951',
     source         => 'https://raw.github.com/ahayworth/home-assistant/ahayworth-schlage-locks/homeassistant/components/lock/zwave.py',
     require        => File[$config_dir],
   }
@@ -19,5 +19,11 @@ class homeassistant::config(
     checksum_value  => '90cc07ae153f9b0e67c11ce807ca18cf',
     source          => 'https://raw.github.com/ahayworth/home-assistant/ahayworth-update-awair/homeassistant/components/sensor/awair.py',
     require         => File[$config_dir],
+  }
+  file { "${config_dir}/custom_components/google_assistant/trait.py":
+    ensure         => present,
+    checksum_value => 'b5bfacf237a57ec2839a1b3ce9196c29',
+    source         => 'https://raw.github.com/ahayworth/home-assistant/ahayworth-google-locks/homeassistant/components/google_assistant/trait.py',
+    require        => File[$config_dir],
   }
 }
