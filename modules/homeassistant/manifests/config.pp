a9ed22b1c8bf0d4c283260aa34c3e472dabcab49
Andrew Hayworth <ahayworth@gmail.com>
2019-06-29 20:06:38 -0500

    Switch to eyaml

--- modules/homeassistant/manifests/config.pp
+++ modules/homeassistant/manifests/config.pp
@@ -1,8 +1,10 @@
 class homeassistant::config(
   String $config_dir,
   String $homeassistant_dir = lookup('homeassistant::homeassistant_dir'),
+  String $xbox_json,
   Hash $custom_components = lookup('homeassistant::custom_components', {}),
   Hash $remote_serial_connections = lookup('homeassistant::remote_serial_connections', {}),
+  Hash $secrets,
 ){
   file { $config_dir:
     ensure  => directory,
@@ -15,6 +17,16 @@ class homeassistant::config(
   file { "${config_dir}/custom_components":
     ensure => link,
     target => "${homeassistant_dir}/custom_components",
+  }
+
+  file { "${config_dir}/xbox_tokens":
+    ensure  => present,
+    content => $xbox_json,
+  }
+
+  file { "${config_dir}/secrets.yaml":
+    ensure  => present,
+    content => to_yaml($secrets),
   }
 
   $custom_components.each |String $name, Hash $options| {
