528399a5b01f98a0a9e9819422f90f18deee3450
Andrew Hayworth <ahayworth@gmail.com>
2019-07-22 23:19:32 -0500

    docker update for buster

--- modules/docker_common/manifests/init.pp
+++ modules/docker_common/manifests/init.pp
@@ -1,5 +1,5 @@
 class docker_common (
-  String $docker_version = "5:18.09.3~3-0~debian-${facts['os']['distro']['codename']}",
+  String $docker_version = "5:19.03.0~3-0~debian-${facts['os']['distro']['codename']}",
   String $compose_version = '1.23.2',
 ){
   class { 'docker':
